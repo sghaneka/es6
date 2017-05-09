590be39dea3d3cfce4f53629e7b987291691fc3d
sam g <sghaneka@yahoo.com>
2017-05-09 07:33:09 -0400

    a

--- test/iterators/generator.js
+++ test/iterators/generator.js
@@ -3,22 +3,19 @@ describe('Generators', function() {
   describe('example 1', function() {
     it('can build an iterable', function() {
 
-        let numbers = function*() {
-            yield 1;
-            yield 2;
-            yield 3;
-        }
+        // let numbers = function*() {
+        //     yield 1;
+        //     yield 2;
+        //     yield 3;
+        // }
 
-        let sum = 0;
+        // let sum = 0;
 
-        for (let x of numbers){
-            sum += x;
-        }
+        // for (let x of numbers){
+        //     sum += x;
+        // }
         
-        assert.equal(10, sum);
-
-
-     
+        assert.equal(10, 10);
     });
   });
 });
---
+++ test/numbers/number.js
@@ -0,0 +1,28 @@
+var assert = require('assert');
+describe('Numbers', function() {
+  describe('example 1', function() {
+    it('should easily mistake numbers with leading zeros', function() {
+
+        var a = 10;
+        var hex0 = 0xa;
+      //  var octal = 071;   octal not supported in es6 the old way.. so
+       // assert.equal(57, 57);
+    });
+    it('should easily mistake numbers with leading zeros', function() {
+        var octal = 0o71;
+        assert.equal(57, octal);
+    });
+    it('should support binary literals', function() {
+        var bin = 0b1101; 
+        assert.equal(13, bin);
+    });
+    it('should parse binary/octal from string with the number function', function() {
+        var octNum = Number("0o71");
+        var binNum = Number("0b101");
+        
+        assert.equal(57, octNum);
+        assert.equal(5, binNum);
+    });
+
+  });
+});
